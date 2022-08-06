2597f3c8ad0d9f934f7f46afac14f3846acd942c
gul-amd-moz <100712631+gul-am@users.noreply.github.com>
2022-08-07 00:55:13 +0200

    fix: auth is killing me, it doesn't work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="https://cdn.tailwindcss.com"></script>
         <title>Stock Controll</title>
     </head>
     <body>
         <div class="bg-gray-100 flex h-screen justify-center items-center">
+
             <form method="post" action="{{ route('submit_login') }}" class="bg-white shadow-md p-8 rounded">
                 @csrf
                 @method('POST')
@@ -19,15 +19,21 @@
                 </div>
                 <div>
                     <label for="">Password</label>
-                    <input type="password" class="shadow-sm appearence-none border rounded w-full p-3" placeholder="******" name="password">
+                    <input type="password" class="shadow-sm appearence-none border rounded w-full p-3" placeholder="****" name="password">
                 </div>
 
                 <div class="mt-4 mb-2">
-                    <div class="w-full p-2 border-2 border-red-500 text-white rounded-sm shadow">Error text</div>
+                    @error('email')
+                    <div class="w-full p-2 bg-red-200 text-xs rounded-sm shadow">{{ $message }}</div>
+                    @enderror
+
+                    @error('password')
+                    <div class="w-full p-2 bg-red-200 text-xs rounded-sm shadow">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <div>
-                    <button type="submit" class="w-full p-3 shadow-sm rounded-sm text-white bg-blue-300 mt-2">Login</button>
+                    <button type="submit" class="w-full p-3 shadow-sm rounded-sm text-white bg-blue-300 mt-2 hover:border-2 hover:border-blue-300 hover:bh-white">Login</button>
                 </div>
             </form>
         </div>
